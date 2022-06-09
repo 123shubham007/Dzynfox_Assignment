1e2bd12737cffb53cb89b6ff444b1cc08eadbfbf
Shubham <shubhamkumar.123.sk001@gmail.com>
2022-06-09 18:30:34 +0530

    init

---
+++ nextjs_landing_page/pages/_app.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/introduction.css';
+import '../styles/feature.css';
+import '../styles/globals.css';
+import '../styles/header.css';
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps }
+    />
+}
+
+export default MyApp
